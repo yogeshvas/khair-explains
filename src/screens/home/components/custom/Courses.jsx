0e74190fd70b37ef72e7cbb88c2bf9a888c276b8
Yogesh Vashisth <writetokhair@gmail.com>
2024-07-05 10:08:54 +0530

    added link preview

--- src/screens/home/components/custom/Courses.jsx
+++ src/screens/home/components/custom/Courses.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { EvervaultCardDemo } from "../shared/CourseCard";
+import LinkPreview from "@/components/ui/link-preview";
 
 const Courses = () => {
   return (
     <>
       <div className="w-full items-center flex justify-center font-bold text-3xl pt-10">
-        Our Courses
+        <p className="text-neutral-500 dark:text-neutral-400 text-xl md:text-3xl max-w-3xl  text-left mb-10">
+          Best{" "}
+          <LinkPreview
+            url="/"
+            className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500"
+          >
+            Cources
+          </LinkPreview>{" "}
+          curated for you.
+        </p>
       </div>
-      <div className="md:flex p-10 md:p-10 md:px-48 gap-3 items-center ">
+      <div className="md:flex p-10 gap-3 items-center justify-center ">
         <EvervaultCardDemo
           title={"DSA"}
           desc={"Data Structure and Algorithm but better"}
--- src/screens/home/components/custom/HeroSection.jsx
+++ src/screens/home/components/custom/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       titleComponent={
         <>
           <h1 className="text-4xl font-semibold text-black dark:text-white">
-            I help to build your ideas, <br />
+            I help to build your thoughts, <br />
             <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
               khair Explains!
             </span>
@@ -20,7 +20,7 @@ const HeroSection = () => {
         alt="hero"
         height={"auto"}
         width={"auto"}
-        className="mx-auto rounded-2xl object-cover h-full object-left-top"
+        className="mx-auto rounded-2xl sm:object-cover h-full object-scale-down"
         draggable={false}
       />
     </ContainerScroll>
