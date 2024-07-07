124618479e551b54f7468318b78ce058a967ec69
Yogesh Vashisth <writetokhair@gmail.com>
2024-07-08 01:38:19 +0530

    video fixed

--- src/screens/chapter/Chapter.jsx
+++ src/screens/chapter/Chapter.jsx
@@ -44,9 +44,9 @@ const Chapter = () => {
   if (error) return <div>Error loading course: {error.message}</div>;
 
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen">
       <div className="flex">
-        <div className="w-1/5 h-screen border-r-2 lg:block hidden lg:flex-col">
+        <div className="w-1/5 border-r-2 lg:block hidden lg:flex-col">
           <div className="w-full items-center justify-center border-b-2">
             <div className="text-2xl font-bold px-6 pt-7 pb-3">
               {data.title}!
@@ -77,7 +77,160 @@ const Chapter = () => {
           </div>
 
           {/* Chapter accordion */}
-          <div className="h-[80%] overflow-scroll">
+          <div className="h-lvh overflow-scroll">
+            {data.chapters.map((video) => (
+              <div
+                key={video._id}
+                className="px-4 py-4 border-b-2 cursor-pointer hover:underline"
+                onClick={() => handleVideoClick(video.videoUrl)}
+              >
+                <div className="flex gap-2 items-center">
+                  <div className="">
+                    {selectedVideoId ===
+                    new URL(video.videoUrl).searchParams.get("v") ? (
+                      <Pause size={20} />
+                    ) : (
+                      <Play size={20} />
+                    )}
+                  </div>
+                  <div className="text-base">{video.title}</div>
+                </div>
+              </div>
+            ))}
+            {data.chapters.map((video) => (
+              <div
+                key={video._id}
+                className="px-4 py-4 border-b-2 cursor-pointer hover:underline"
+                onClick={() => handleVideoClick(video.videoUrl)}
+              >
+                <div className="flex gap-2 items-center">
+                  <div className="">
+                    {selectedVideoId ===
+                    new URL(video.videoUrl).searchParams.get("v") ? (
+                      <Pause size={20} />
+                    ) : (
+                      <Play size={20} />
+                    )}
+                  </div>
+                  <div className="text-base">{video.title}</div>
+                </div>
+              </div>
+            ))}
+            {data.chapters.map((video) => (
+              <div
+                key={video._id}
+                className="px-4 py-4 border-b-2 cursor-pointer hover:underline"
+                onClick={() => handleVideoClick(video.videoUrl)}
+              >
+                <div className="flex gap-2 items-center">
+                  <div className="">
+                    {selectedVideoId ===
+                    new URL(video.videoUrl).searchParams.get("v") ? (
+                      <Pause size={20} />
+                    ) : (
+                      <Play size={20} />
+                    )}
+                  </div>
+                  <div className="text-base">{video.title}</div>
+                </div>
+              </div>
+            ))}
+
+            {data.chapters.map((video) => (
+              <div
+                key={video._id}
+                className="px-4 py-4 border-b-2 cursor-pointer hover:underline"
+                onClick={() => handleVideoClick(video.videoUrl)}
+              >
+                <div className="flex gap-2 items-center">
+                  <div className="">
+                    {selectedVideoId ===
+                    new URL(video.videoUrl).searchParams.get("v") ? (
+                      <Pause size={20} />
+                    ) : (
+                      <Play size={20} />
+                    )}
+                  </div>
+                  <div className="text-base">{video.title}</div>
+                </div>
+              </div>
+            ))}
+            {data.chapters.map((video) => (
+              <div
+                key={video._id}
+                className="px-4 py-4 border-b-2 cursor-pointer hover:underline"
+                onClick={() => handleVideoClick(video.videoUrl)}
+              >
+                <div className="flex gap-2 items-center">
+                  <div className="">
+                    {selectedVideoId ===
+                    new URL(video.videoUrl).searchParams.get("v") ? (
+                      <Pause size={20} />
+                    ) : (
+                      <Play size={20} />
+                    )}
+                  </div>
+                  <div className="text-base">{video.title}</div>
+                </div>
+              </div>
+            ))}
+            {data.chapters.map((video) => (
+              <div
+                key={video._id}
+                className="px-4 py-4 border-b-2 cursor-pointer hover:underline"
+                onClick={() => handleVideoClick(video.videoUrl)}
+              >
+                <div className="flex gap-2 items-center">
+                  <div className="">
+                    {selectedVideoId ===
+                    new URL(video.videoUrl).searchParams.get("v") ? (
+                      <Pause size={20} />
+                    ) : (
+                      <Play size={20} />
+                    )}
+                  </div>
+                  <div className="text-base">{video.title}</div>
+                </div>
+              </div>
+            ))}
+            {data.chapters.map((video) => (
+              <div
+                key={video._id}
+                className="px-4 py-4 border-b-2 cursor-pointer hover:underline"
+                onClick={() => handleVideoClick(video.videoUrl)}
+              >
+                <div className="flex gap-2 items-center">
+                  <div className="">
+                    {selectedVideoId ===
+                    new URL(video.videoUrl).searchParams.get("v") ? (
+                      <Pause size={20} />
+                    ) : (
+                      <Play size={20} />
+                    )}
+                  </div>
+                  <div className="text-base">{video.title}</div>
+                </div>
+              </div>
+            ))}
+            {data.chapters.map((video) => (
+              <div
+                key={video._id}
+                className="px-4 py-4 border-b-2 cursor-pointer hover:underline"
+                onClick={() => handleVideoClick(video.videoUrl)}
+              >
+                <div className="flex gap-2 items-center">
+                  <div className="">
+                    {selectedVideoId ===
+                    new URL(video.videoUrl).searchParams.get("v") ? (
+                      <Pause size={20} />
+                    ) : (
+                      <Play size={20} />
+                    )}
+                  </div>
+                  <div className="text-base">{video.title}</div>
+                </div>
+              </div>
+            ))}
             {data.chapters.map((video) => (
               <div
                 key={video._id}
