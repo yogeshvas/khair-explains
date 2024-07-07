f783453027858ad02bf2fd885af7144fb2cda5de
Yogesh Vashisth <writetokhair@gmail.com>
2024-07-07 18:18:49 +0530

    done

--- src/service/khair.js
+++ src/service/khair.js
@@ -11,26 +11,30 @@ const apiClient = axios.create({
 export default apiClient;
 
 export const getAllCourses = async () => {
-  const response = await apiClient.get("/courses");
+  const response = await apiClient.get("/v1/courses");
   return response.data;
 };
 
 export const getACourse = async (id) => {
-  const response = await apiClient.get(`/courses/${id}`);
+  const response = await apiClient.get(`/v1/courses/${id}`);
   return response.data;
 };
 
 export const login = async (email, password) => {
-  const response = await apiClient.post("/auth/login", { email, password });
+  const response = await apiClient.post("/v1/auth/login", { email, password });
   return response.data;
 };
 
 export const signup = async (name, email, password) => {
-  const response = await apiClient.post("/signup", { name, email, password });
+  const response = await apiClient.post("/v1/signup", {
+    name,
+    email,
+    password,
+  });
   return response.data;
 };
 
 export const getMe = async () => {
-  const response = await apiClient.get("/me");
+  const response = await apiClient.get("/v1/me");
   return response.data;
 };
